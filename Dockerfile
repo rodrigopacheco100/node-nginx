261f1517d8df3c420c5f70915e1eb945e38834dd
Rodrigo Pacheco <rodrigopacheco100@gmail.com>
2023-02-27 15:43:55 -0300

    feat: :tada: first commit

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM node:18 AS builder
+
+RUN apt-get update && apt-get install -y wget
+
+ENV DOCKERIZE_VERSION v0.6.1
+RUN wget https://github.com/jwilder/dockerize/releases/download/$DOCKERIZE_VERSION/dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz \
+    && tar -C /usr/local/bin -xzvf dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz \
+    && rm dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz
+
+COPY src .
+COPY package.json .
+COPY package-lock.json .
+COPY prisma .
+
+RUN npm install
+
+EXPOSE 8080
+
+ENTRYPOINT [ "node" ]
---
+++ src/index.js
@@ -0,0 +1,40 @@
+const fastify = require("fastify")
+const { faker } = require('@faker-js/faker')
+const { PrismaClient } = require('@prisma/client')
+
+const server = fastify()
+
+const prisma = new PrismaClient({
+  log: ['query']
+})
+
+server.get("/", async () => {
+  const newName = faker.name.fullName()
+
+  await prisma.people.create({
+    data: {
+      name: newName
+    }
+  })
+
+  const people = await prisma.people.findMany({
+    select: {
+      name: true
+    }
+  })
+
+  const nameList = people.reduce((acc, person) => {
+    return acc + `<li>${person.name}</li>
+    `
+  }, '')
+
+  return `
+          <h1>Full Cycle Rocks!</h1>
+          <ul>
+            ${nameList}</ul>
+          `
+})
+
+server.listen({ port: 8080, host: '0.0.0.0' }).then((uri) => {
+  console.log('App is running at', uri)
+})
